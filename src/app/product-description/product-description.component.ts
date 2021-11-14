238612270811d85065e53c19d03da28f4e742a61
Daniel Amusan <86704130+DanielAmusan@users.noreply.github.com>
2021-11-14 23:28:24 +0100

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -14,7 +14,7 @@ export class ProductDescriptionComponent implements OnInit {
 
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response =>
-    this.albuminfo = response);
+    this.albumInfo = response);
 
   }
 

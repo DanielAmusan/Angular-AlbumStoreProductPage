3e33b3a2f4087572119f439c61f023cd8d4addf5
Daniel Amusan <86704130+DanielAmusan@users.noreply.github.com>
2021-11-15 23:17:36 +0100

    Update product-tracklisting.component.ts

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ProductService } from '../album';
+import {ProductService } from '../product.service';
 import { Album } from '../album';
 
 @Component({
